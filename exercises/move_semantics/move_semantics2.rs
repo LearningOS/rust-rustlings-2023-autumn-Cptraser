bc8214a592f165bf18d35de20bd2cbc351dac255
Cptraser <39483928+Cptraser@users.noreply.github.com>
2023-10-10 15:03:33 +0000

    errors DONE

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -9,7 +9,7 @@
 
 
 fn main() {
-    let vec0 = Vec::new();
+    let vec0 = vec![22, 44, 66];
 
     let mut vec1 = fill_vec(vec0.clone());
 
@@ -23,9 +23,9 @@ fn main() {
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    // vec.push(22);
+    // vec.push(44);
+    // vec.push(66);
 
     vec
 }
